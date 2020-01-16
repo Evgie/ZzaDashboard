1eef742081e8f6263853bd079cb58fc2978e3625
Evgie <tubarao@ukr.net>
2020-01-17 00:22:49 +0200

    Add project files.

---
+++ ZzaDashboard/ViewModel/CustomerEditViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.ViewModel
+{
+    public class CustomerEditViewModel
+    {
+        private ICustomersRepository CustomersRepository { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public CustomerEditViewModel()
+        {
+            this.CustomersRepository = new CustomersRepository();
+            this.Customer = this.CustomersRepository.GetCustomerAsync(new Guid("11DA4696-CEA3-4A6D-8E83-013F1C479618")).Result;
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        public void Execute()
+        {
+
+        }
+    }
+}
