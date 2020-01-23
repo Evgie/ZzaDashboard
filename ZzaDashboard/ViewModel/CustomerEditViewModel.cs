c856a2caf2999ce5f590feca347a196b27c92d30
Evgie <tubarao@ukr.net>
2020-01-23 17:06:32 +0200

    1

--- ZzaDashboard/ViewModel/CustomerEditViewModel.cs
+++ ZzaDashboard/ViewModel/CustomerEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,23 +11,82 @@ using ZzaDashboard.Services;
 
 namespace ZzaDashboard.ViewModel
 {
-    public class CustomerEditViewModel
+    public class CustomerEditViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Customer> customers;
+
+        public ObservableCollection<Customer> Customers
+        {
+            get
+            {
+                return this.customers;
+            }
+            set
+            {
+                if (this.customers == value)
+                    return;
+
+                this.customers = value;
+                this.OnPropertyChanged(nameof(this.Customers));
+            }
+        }
+
+        //public ObservableCollection<Customer> Customers { get; set; }
+
         private ICustomersRepository CustomersRepository { get; set; }
 
-        public Customer Customer { get; set; }
+        private Customer customer;
+
+        public Customer Customer
+        {
+            get
+            {
+                return this.customer;
+            }
+            set
+            {
+                if (this.customer == value)
+                    return;
+                
+                this.customer = value;
+                this.OnPropertyChanged(nameof(this.Customer));
+            }
+        }
 
         public CustomerEditViewModel()
         {
             if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
                 return;
-
+            //this.Customers = new ObservableCollection<Customer>()
+            //{
+            //    new Customer{FirstName = "Tetyana", LastName = "Ryzhik", Phone = "123"},
+            //    new Customer{FirstName = "Andrew", LastName = "Fedorchenko", Phone = "321"},
+            //    new Customer{FirstName = "Vlad", LastName = "Radchenko", Phone = "567"},
+            //};
             this.CustomersRepository = new CustomersRepository();
             this.Customer = this.CustomersRepository.GetCustomerAsync(new Guid("11DA4696-CEA3-4A6D-8E83-013F1C479618")).Result;
             this.SaveCommand = new RelayCommand(this.SaveExecute, this.SaveCanExecute);
+            this.Customers = new ObservableCollection<Customer>();
+            //this.InitializeCustomers();
         }
 
+        //public async void InitializeCustomers(object obj)
+        //{
+
+        //    this.Customers = await CustomersRepository.GetCustomersAsync();
+        //}
+
         public ICommand SaveCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged is null)
+                return;
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public async void SaveExecute(object obj)
         {
